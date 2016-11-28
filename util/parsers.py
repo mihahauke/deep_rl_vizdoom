ed20f34c3bb29a09911f4e545bd1e6bb102152c8
michał <kempka.michal@gmail.com>
2016-11-29 00:56:59 +0100

    Moved a3c settings to settings/a3c

--- util/parsers.py
+++ util/parsers.py
@@ -3,7 +3,9 @@ import argparse
 
 def parse_train_a3c_args():
     parser = argparse.ArgumentParser(description='A3c implementation for ViZDoom.')
-
+    parser.add_argument("--json", "-j", dest="settings_json", metavar='<SETTINGS_JSON_FILE>',
+                        default="settings/a3c/basic.json",
+                        type=str, required=False, help="path to json with settings")
     # TODO run
     # TODO tag
     # TODO settings
